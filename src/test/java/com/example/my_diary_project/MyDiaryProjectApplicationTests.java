131539e4f5b099ddba788d5d6b0ec16b39b757dd
Jin-hyeok2 <hjhlove1120@gmail.com>
2023-06-21 00:25:58 +0900

    init project

---
+++ src/test/java/com/example/my_diary_project/MyDiaryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_diary_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDiaryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
